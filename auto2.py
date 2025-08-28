23af5e0c29589644b384bad910f86eae9310368e
all4fun123 <59323993+all4fun123@users.noreply.github.com>
2025-08-28 18:49:07 +0700

    Add files via upload

--- auto2.py
+++ auto2.py
@@ -166,8 +166,8 @@ async def run_event_flow(username, key, state):
     limits = httpx.Limits(max_connections=1, max_keepalive_connections=1, keepalive_expiry=0)
     # Define proxy (e.g., HTTP or HTTPS proxy)
     proxies = {
-        "http://": "http://103.67.199.104:20151",
-        "https://": "http://103.67.199.104:20151"  # Use the same or different proxy for HTTPS
+        "http://": "http://160.191.48.160:6588",
+        "https://": "http://160.191.48.160:6588"  # Use the same or different proxy for HTTPS
     }
     async with httpx.AsyncClient(timeout=3.0, http2=False, limits=limits, proxies=proxies) as client:
         retry_count = 0
