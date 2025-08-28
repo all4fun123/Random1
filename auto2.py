7c54693077c63948f67fe7186c11b86a22ff3b36
all4fun123 <59323993+all4fun123@users.noreply.github.com>
2025-08-29 03:00:35 +0700

    Update auto2.py

--- auto2.py
+++ auto2.py
@@ -166,8 +166,8 @@ async def run_event_flow(username, key, state):
     limits = httpx.Limits(max_connections=1, max_keepalive_connections=1, keepalive_expiry=0)
     # Define proxy (e.g., HTTP or HTTPS proxy)
     proxies = {
-        "http://": "http://160.191.48.160:10004",
-        "https://": "http://160.191.48.160:10004"  # Use the same or different proxy for HTTPS
+        "http://": "http://160.191.49.251:20052",
+        "https://": "http://160.191.49.251:20052"  # Use the same or different proxy for HTTPS
     }
     async with httpx.AsyncClient(timeout=3.0, http2=False, limits=limits, proxies=proxies) as client:
         retry_count = 0
@@ -398,4 +398,5 @@ async def main():
         logger.info("Nhận tín hiệu dừng, đang thoát...")
 
 if __name__ == "__main__":
+
     asyncio.run(main())
